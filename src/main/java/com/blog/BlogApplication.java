a63b33224b59b0f03beab5ccf2870a8f9ffb880a
Arar Taha <tarar@wissalgroup.com>
2025-10-11 15:01:55 +0100

    upgrade article entity

--- src/main/java/com/blog/BlogApplication.java
+++ src/main/java/com/blog/BlogApplication.java
@@ -2,7 +2,9 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.blog.model"})
 @SpringBootApplication
 public class BlogApplication {
 
