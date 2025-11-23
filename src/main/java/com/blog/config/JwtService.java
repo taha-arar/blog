f543e3acff08f671f90d7b0706505eb627757af1
Arar Taha <tarar@wissalgroup.com>
2025-11-23 10:15:35 +0100

    enhance JWT service: add token validation, expiration checks, username extraction, and claims processing methods

--- src/main/java/com/blog/config/JwtService.java
+++ src/main/java/com/blog/config/JwtService.java
@@ -1,5 +1,6 @@
 package com.blog.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -9,10 +10,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.sql.Date;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -45,5 +47,37 @@ public class JwtService {
     private SecretKey getSigningKey(){
         byte [] keyBytes = jwtProperties.getSecret().getBytes();
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return  extract(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extract(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+
+    public <T> T extract(String token, Function<Claims,T> claimsTResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsTResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
